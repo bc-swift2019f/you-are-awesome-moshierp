c39d4e4afc52f7c231a5737eb2413a8aeac80142
Paige Moshier <moshierp@bc.edu>
2019-09-03 22:19:44 -0400

    2.3 Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Paige Moshier on 9/3/19.
+//  Copyright © 2019 Paige Moshier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    // Code below executes when the app's view first loads
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+        
+    }
+}
+
